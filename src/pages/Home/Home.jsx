9eb4f86ddf5ef837abc5a3ee18eb7b3ba6c6b15d
Fahad Hasan <44fahadhasan@gmail.com>
2024-04-15 08:26:35 +0600

    added need table componet in the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import NeedTable from "../shared/NeedTable/NeedTable";
 import PopularCoffee from "./PopularCoffee/PopularCoffee";
 import Slider from "./Slider/Slider";
 
@@ -7,9 +8,12 @@ const Home = () => {
       <section className="overflow-hidden block">
         <Slider />
       </section>
+
       <section className="bg-[url('https://i.ibb.co/SsbwvS9/category-bg.jpg')] bg-cover	bg-no-repeat bg-center py-[130px] bg-fixed">
         <PopularCoffee />
       </section>
+
+      <NeedTable />
     </div>
   );
 };
