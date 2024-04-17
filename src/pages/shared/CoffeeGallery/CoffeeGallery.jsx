bba8e138c830ac8d33265445761adbade5aa7a3a
Fahad Hasan <44fahadhasan@gmail.com>
2024-04-17 10:55:47 +0600

    img gallery comonent updated and added img json data

--- src/pages/shared/CoffeeGallery/CoffeeGallery.jsx
+++ src/pages/shared/CoffeeGallery/CoffeeGallery.jsx
@@ -1,14 +1,11 @@
 import { Autoplay, FreeMode, Keyboard } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import useDataLoader from "../../../hooks/useDataLoader";
 import CoffeGallerySingleImg from "../CoffeGallerySingleImg/CoffeGallerySingleImg";
 
-import imgOne from "../../../assets/images/grid_gal1.jpg";
-import imgTwo from "../../../assets/images/grid_gal2.jpg";
-import imgThree from "../../../assets/images/grid_gal3.jpg";
-import imgFour from "../../../assets/images/grid_gal4.jpg";
-import imgFive from "../../../assets/images/grid_gal5.jpg";
-
 const CoffeeGallery = () => {
+  const galleryDatas = useDataLoader("/public/galleryImgData.json");
+
   return (
     <div className="bg-base-100 py-[130px]">
       <div className="container mx-auto w-[85%] ">
@@ -26,24 +23,18 @@ const CoffeeGallery = () => {
           modules={[Autoplay, FreeMode, Keyboard]}
         >
           {/* single card */}
-          <SwiperSlide className="mt-10">
-            <CoffeGallerySingleImg img={imgOne} text="Black Coffee" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CoffeGallerySingleImg img={imgTwo} text="Espresso" />
-          </SwiperSlide>
-          <SwiperSlide className="self-end">
-            <CoffeGallerySingleImg img={imgThree} text="Iced Coffee" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CoffeGallerySingleImg img={imgFour} text="Latte" />
-          </SwiperSlide>
-          <SwiperSlide className="self-end">
-            <CoffeGallerySingleImg img={imgFive} text="Cappuccino" />
-          </SwiperSlide>
-          {/* single card */}
+          {galleryDatas.map((galleryData) => (
+            <SwiperSlide
+              key={galleryData.id}
+              className={`${galleryData.id % 2 === 0 ? "mt-0" : "mt-12"}`}
+            >
+              <CoffeGallerySingleImg
+                img={galleryData.image}
+                text={galleryData.coffeeName}
+              />
+            </SwiperSlide>
+          ))}
         </Swiper>
-        {/* Swiper end */}
       </div>
     </div>
   );
