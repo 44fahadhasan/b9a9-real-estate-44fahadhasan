db80c947d818851183f3ed93e38aa2ed9d13e075
Fahad Hasan <44fahadhasan@gmail.com>
2024-04-17 16:30:52 +0600

    added showup section of website

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,6 +17,53 @@ const Home = () => {
         <AboutUs />
       </section>
 
+      <section className="bg-base-100 py-[70px] drop-shadow-sm border-b-2 border-base-200">
+        <div className="grid justify-items-center md:grid-cols-2 lg:grid-cols-3 gap-9 container mx-auto w-[85%]">
+          <div className="flex gap-6 mt-8">
+            <div className="basis-[20%]">
+              <h1 className="oswald text-[50px] sm:text-[60px] text-primary font-light">
+                256+
+              </h1>
+            </div>
+            <div className="basis-[80%]">
+              <h2 className="text-secondary text-2xl oswald">
+                Premium Clients
+              </h2>
+              <p className="roboto text-secondary-content mt-3">
+                Exceptional service, and a premium experience.
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-6 mt-8">
+            <div className="basis-[20%]">
+              <h1 className="oswald text-[50px] sm:text-[60px] text-primary font-light">
+                362+
+              </h1>
+            </div>
+            <div className="basis-[80%]">
+              <h2 className="text-secondary text-2xl oswald">Expert Members</h2>
+              <p className="roboto text-secondary-content mt-3">
+                Expert members in a coffee shop are the backbone of the
+                establishment
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-6 mt-8">
+            <div className="basis-[20%]">
+              <h1 className="oswald text-[50px] sm:text-[60px] text-primary font-light">
+                753+
+              </h1>
+            </div>
+            <div className="basis-[80%]">
+              <h2 className="text-secondary text-2xl oswald">Winning Awards</h2>
+              <p className="roboto text-secondary-content mt-3">
+                Dedication to providing for exceptional customer experience
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <section className="bg-[url('https://i.ibb.co/SsbwvS9/category-bg.jpg')] bg-cover	bg-no-repeat bg-center py-[130px] bg-fixed">
         <PopularCoffee />
       </section>
